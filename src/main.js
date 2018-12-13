0ff0ca82c4c4f3051f30855196fba742c413be75
weebo <13151558707@163.com>
2018-12-13 15:40:03 +0800

    inittwo

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import Mintui from ''
-
-Vue.config.productionTip = false
 
 new Vue({
-  router,
   render: h => h(App),
 }).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
-import VueRouter from 'vue-router'
+// import VueRouter from 'vue-router'
 
-// 3. 创建路由对象
-var router = new VueRouter({
-  routes: [
-  ]
-})
+// // 3. 创建路由对象
+// var router = new VueRouter({
+//   routes: [
+//   ]
+// })
 
-// 把路由对象暴露出去
-export default router
+// // 把路由对象暴露出去
+// export default router
